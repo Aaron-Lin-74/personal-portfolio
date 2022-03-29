aed91c97df30a6d089940120e82822345f6b86c1
Aaron Lin <aaronlin74@gmail.com>
2022-03-29 15:49:03 +1100

    feat(app): set up App component, update its test and style
    Added Header component to the App, set up global styles in App.scss which replaced App.css. Update
    the auto generated app test.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
