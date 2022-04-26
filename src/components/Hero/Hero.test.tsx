7b21d1cced440af253e23b6acde6413b60d3ad50
Aaron Lin <aaronlin74@gmail.com>
2022-04-26 21:26:24 +1000

    test: update the Hero component test

--- src/components/Hero/Hero.test.tsx
+++ src/components/Hero/Hero.test.tsx
@@ -1,6 +1,5 @@
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 import Hero from './Hero';
 
 describe('Hero component', () => {
@@ -9,9 +8,6 @@ describe('Hero component', () => {
     screen.getByText(/hi, i am aaron/i);
     screen.getByRole('img', {
       name: /my profile/i,
-    });
-    screen.getByRole('heading', {
-      name: /i am a highly motivated frontend developer who is passionate about web development\./i,
     });
     screen.getByText(
       /my experiences include front\-end web development \(react, redux, javascript es6\/typescript, html5, and css3\/sass\), back\-end web development \(node\/express, rest api\), and aws cloud infrastructure\./i
@@ -25,11 +21,11 @@ describe('Hero component', () => {
     expect(screen.getAllByRole('link')[0]).toHaveAttribute('href', '#contact');
     expect(screen.getAllByRole('link')[1]).toHaveAttribute(
       'href',
-      'Aaron(Yaotian)_Lin_Resume.pdf'
+      'Aaron_Lin_Resume.pdf'
     );
     expect(screen.getAllByRole('link')[1]).toHaveAttribute(
       'download',
-      'Aaron(Yaotian)_Lin_Resume.pdf'
+      'Aaron_Lin_Resume.pdf'
     );
   });
 });
